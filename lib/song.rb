cdafd5773a9131bd1ab3a121df65cecbcaa87e14
Andrew Smith <848smith@gmail.com>
2020-02-15 07:15:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,45 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song.name = song
+  end
+  
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end
+  
+  def self.create_by_name(title)
+    song = self.new
+    song.name = title
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(title)
+    @@all.find.each do |song|
+      song.name == title
+    end
+  end
+  
+  def self.find_or_create_by_name(title)
+    if self.find_by_name(title)
+      self.find_by_name(title)
+    else
+      create_by_name(title)
+    end
+  end
 end
+
+
+
+
+
+
+
+
